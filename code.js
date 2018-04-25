a47e15669f033ccc283774414f8e369cc6e799cd
Inuuko <34916730+Inuuko@users.noreply.github.com>
2018-04-25 18:09:57 +0200

    java

--- code.js
+++ code.js
@@ -1,2 +1,27 @@
 
-// add js here
+    const result = [];
+
+    const losowanie = function() {
+
+
+      const wynikLosowania = Math.floor(Math.random() * 6 + 1);
+
+
+      const div = document.createElement("div");
+	    div.id = "cube";
+     
+      div.textContent = wynikLosowania;
+      
+  
+      result.push(wynikLosowania); 
+      
+
+	document.getElementById("cube-box").appendChild(div);
+    }
+
+
+
+    const button = document.querySelector("button");
+
+
+    button.addEventListener("click", losowanie);
