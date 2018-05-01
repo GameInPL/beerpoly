ab460d4aca61bb5d7b54f0ece96d5a9e5faa3b1d
Inuuko <34916730+Inuuko@users.noreply.github.com>
2018-05-01 17:54:43 +0200

    js png

--- code.js
+++ code.js
@@ -1,27 +1,67 @@
 
-    const result = [];
+	//    Tablica przechowująca wyniki
+    const result1 = [];{
 
+	//  funkcja odpowiedzialna za losowanie i umieszczanie liczby na stronie internetowej
     const losowanie = function() {
 
+	//losowanie liczny 
+    const wynikLosowania = Math.floor(Math.random() * 6 + 1);
 
-      const wynikLosowania = Math.floor(Math.random() * 6 + 1);
-
-
+   
+	//tworzymy element div, ktory dodamy do dokumsntu (strony)
       const div = document.createElement("div");
-	    div.id = "cube";
-     
+	    div.className = "cube1";
+		
+		
       div.textContent = wynikLosowania;
       
-  
-      result.push(wynikLosowania); 
+      //dodajemy do tablicy wyników (result)
+      result1.push(wynikLosowania); 
       
-
+    //dodaje div do elementu 
 	document.getElementById("cube-box").appendChild(div);
     }
 
 
+    //Pobranie przycisku do zmiennej
+    const button = document.querySelector("button");
+
+    //nasłuchiwanie na zdarzenie 
+    button.addEventListener("click", losowanie);
+	
+	}
+	
+	
+//2x to samo	
+
+    const result2 = [];{
+    const losowanie = function() {
+
+      const wynikLosowania = Math.floor(Math.random() * 6 + 1);
+
+      const div = document.createElement("div");
+	    div.className = "cube2";
+		
+      div.textContent = wynikLosowania;
+      
+      result2.push(wynikLosowania); 
+      
+	document.getElementById("cube-box").appendChild(div);
+    }
 
     const button = document.querySelector("button");
 
-
     button.addEventListener("click", losowanie);
+	
+	}
+	
+	
+	if(result1==6 && result2==6) {
+	
+	}
+	
+	
+	
+	
+	
