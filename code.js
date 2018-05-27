0573df474b647a120dc1258cb995a01dee461dd6
Sebastian Pożoga <prog255@gmail.com>
2018-05-27 22:50:52 +0200

    correct js loading (load js at end of html body)

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 (function init() {
-  const button = document.querySelector('body > button');
+  const button = document.getElementById('rollOfDicesButton');
   button.addEventListener('click', rollOfDices);
   window.addEventListener('keydown', moveSelection);
 })();
